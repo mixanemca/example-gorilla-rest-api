71cac5015eac536d022d77b56d5169fb0f2fd360
Anton <l16.rid@gmail.com>
2023-09-22 21:28:04 +0600

    config added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # example-gorilla-rest-api
 Example project with RESTful API on Gorilla mux router
+
+## Help
+You can pass the `--help` flag to see all the flags and description.
+
+```
+Usage of ./example-gorilla-rest-api:
+  -c, --config string            path to config file
+  -H, --database.host string     database host (default "localhost")
+  -N, --database.name string     database name (default "gapi")
+  -P, --database.port int        database port (default 5432)
+  -U, --database.user string     database user (default "postgres")
+  -a, --http.address string      http listening address (default "127.0.0.1:8080")
+  -t, --http.timeout.read int    http read timeout (default 5)
+  -w, --http.timeout.write int   http write timeout (default 5)
+  -f, --log.format log.format    log format (default json)
+  -l, --log.level log.level      log level (default info)
+pflag: help requested
+```
---
+++ internal/config/config.go
@@ -0,0 +1,136 @@
+package config
+
+import (
+	"log/slog"
+	"strings"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"github.com/thediveo/enumflag/v2"
+)
+
+const (
+	json = iota
+	text
+)
+
+var (
+	configPath       string
+	httpAddress      string
+	httpTimeoutRead  int
+	httpTimeoutWrite int
+	logLevel         slog.Level
+	logFormat        int
+	databaseHost     string
+	databasePort     int
+	databaseName     string
+	databaseUser     string
+	databasePassword string
+)
+
+var logLevelIds = map[slog.Level][]string{
+	slog.LevelDebug: {"debug"},
+	slog.LevelInfo:  {"info"},
+	slog.LevelWarn:  {"warning", "warn"},
+	slog.LevelError: {"error"},
+}
+
+var logFormatIds = map[int][]string{
+	json: {"json"},
+	text: {"text"},
+}
+
+type Config struct {
+	HTTP     HTTP     `mapstructure:"http-config"`
+	Logger   Log      `mapstructure:"log"`
+	Database Database `mapstructure:"database"`
+	Version  string
+	Build    string
+}
+
+type HTTP struct {
+	Address string      `mapstructure:"address"`
+	Timeout HTTPTimeout `mapstructure:"timeout"`
+}
+
+type HTTPTimeout struct {
+	Read  int `mapstructure:"read"`
+	Write int `mapstructure:"write"`
+}
+
+type Log struct {
+	Level  string `mapstructure:"level"`
+	Format string `mapstructure:"format"`
+}
+
+type Database struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Name     string `mapstructure:"name"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+}
+
+func New(version, build string) (*Config, error) {
+	// Set enviroment prefix and bind to viper.
+	viper.SetEnvPrefix("EGRA")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	// Scan CLI flags and bind to viper.
+	pflag.StringVarP(&configPath, "config", "c", viper.GetViper().ConfigFileUsed(), "path to config file")
+	pflag.StringVarP(&httpAddress, "http.address", "a", "127.0.0.1:8080", "http listening address")
+	pflag.IntVarP(&httpTimeoutRead, "http.timeout.read", "t", 5, "http read timeout")
+	pflag.IntVarP(&httpTimeoutWrite, "http.timeout.write", "w", 5, "http write timeout")
+	pflag.VarP(
+		enumflag.New(&logLevel, "log.level", logLevelIds, enumflag.EnumCaseSensitive),
+		"log.level", "l", "log level",
+	)
+	pflag.VarP(
+		enumflag.New(&logFormat, "log.format", logFormatIds, enumflag.EnumCaseSensitive),
+		"log.format", "f", "log format",
+	)
+	pflag.StringVarP(&databaseHost, "database.host", "H", "localhost", "database host")
+	pflag.IntVarP(&databasePort, "database.port", "P", 5432, "database port")
+	pflag.StringVarP(&databaseName, "database.name", "N", "gapi", "database name")
+	pflag.StringVarP(&databaseUser, "database.user", "U", "postgres", "database user")
+	pflag.Parse()
+
+	viper.BindPFlag("http.address", pflag.Lookup("http.address"))
+	viper.BindPFlag("http.timeout.read", pflag.Lookup("http.timeout.read"))
+	viper.BindPFlag("http.timeout.write", pflag.Lookup("http.timeout.write"))
+	viper.BindPFlag("log.level", pflag.Lookup("log.level"))
+	viper.BindPFlag("log.format", pflag.Lookup("log.format"))
+	viper.BindPFlag("databse.host", pflag.Lookup("database.host"))
+	viper.BindPFlag("databse.port", pflag.Lookup("database.port"))
+	viper.BindPFlag("databse.name", pflag.Lookup("database.name"))
+	viper.BindPFlag("databse.user", pflag.Lookup("database.user"))
+
+	if configPath != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(configPath)
+	} else {
+		// Set config name, path and type.
+		viper.AddConfigPath("configs")
+		viper.AddConfigPath("/etc/example-gorilla-rest-api")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	// Discover and load the configuration file from disk.
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	// unmarshal unmarshals the config into a Struct
+	var cfg Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.Version = version
+	cfg.Build = build
+
+	return &cfg, nil
+}
