f9a5a6e4bb88f9335fea81eae58b0786d4b123bf
Anton <l16.rid@gmail.com>
2023-09-22 21:28:04 +0600

    config added

---
+++ cmd/example-gorilla-rest-api/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"log/slog"
+
+	"github.com/mixanemca/example-gorilla-rest-api/internal/config"
+	"github.com/mixanemca/example-gorilla-rest-api/internal/logger"
+)
+
+var (
+	version string = "unknown"
+	build   string = "unknown"
+)
+
+func main() {
+	cfg, err := config.New(version, build)
+	if err != nil {
+		log.Fatalf("error occurred while reading config: %s\n", err.Error())
+	}
+	if cfg == nil {
+		slog.Error("config is empty")
+		return
+	}
+
+	// set logger
+	log, err := logger.New(cfg.Logger.Level, cfg.Logger.Format)
+	if err != nil {
+		slog.Error("create logger", slog.String("error", err.Error()))
+		return
+	}
+	log.Info(
+		"starting example-gorilla-rest-api",
+		slog.String("version", cfg.Version),
+		slog.String("build", cfg.Build),
+		slog.String("log level", cfg.Logger.Level),
+		slog.String("log format", cfg.Logger.Format),
+	)
+	log.Debug("debug messages are enabled")
+}
