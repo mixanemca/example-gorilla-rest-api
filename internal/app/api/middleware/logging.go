c7832aec810725e16acde8e3af84e3e8336a5021
Anton <l16.rid@gmail.com>
2023-11-06 20:21:25 +0600

    add validator

--- internal/app/api/middleware/logging.go
+++ internal/app/api/middleware/logging.go
@@ -10,7 +10,20 @@ import (
 	"log/slog"
 )
 
-// LoggingMiddleware middleware for logging requests
+type ResponseWriterWrapper struct {
+	http.ResponseWriter
+	status int
+}
+
+func NewResponseWriterWrapper(w http.ResponseWriter) *ResponseWriterWrapper {
+	return &ResponseWriterWrapper{w, http.StatusOK}
+}
+
+func (w *ResponseWriterWrapper) Status() int {
+	return w.status
+}
+
+// LoggingMiddleware middleware for logging successful requests
 func LoggingMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -19,11 +32,17 @@ func LoggingMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
 				return
 			}
 
-			body, _ := io.ReadAll(r.Body)
-			logger.Info("Request: " + r.Method + " " + r.URL.Path + string(body))
-			r.Body = io.NopCloser(bytes.NewBuffer(body))
+			// create a ResponseWriter Wrapper that intercepts the response status
+			wrapper := NewResponseWriterWrapper(w)
+			next.ServeHTTP(wrapper, r)
+			statusCode := wrapper.Status()
 
-			next.ServeHTTP(w, r)
+			// skip bad request status
+			if statusCode != http.StatusBadRequest {
+				body, _ := io.ReadAll(r.Body)
+				logger.Info("Request: " + r.Method + " " + r.URL.Path + string(body))
+				r.Body = io.NopCloser(bytes.NewBuffer(body))
+			}
 		})
 	}
 }
--- models/user.go
+++ models/user.go
@@ -1,19 +1,15 @@
 // Package models for users
 package models
 
-import (
-	"regexp"
-	"strings"
-)
-
 type Users []UserResponse
+
 type User struct {
 	BaseModel
-	Name     string `json:"name"`
-	Surname  string `json:"surname"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Phone    string `json:"phone"`
+	Name     string `json:"name" validate:"required,alpha,min=3"`
+	Surname  string `json:"surname" validate:"required,alpha,min=3"`
+	Username string `json:"username" validate:"required,alphanum,min=3"`
+	Email    string `json:"email" validate:"required,email"`
+	Phone    string `json:"phone" validate:"required,e164"`
 }
 
 type UserResponse struct {
@@ -25,23 +21,4 @@ type UserResponse struct {
 	Phone    string `json:"phone"`
 }
 
-func (u User) Validate() string {
-	var errMessage []string
-	if re := regexp.MustCompile(`^[A-Za-z]{3,}$`); !re.MatchString(u.Name) {
-		errMessage = append(errMessage, "Name should consist of only English letters with a minimum length of 3 characters.")
-	}
-	if re := regexp.MustCompile(`^[A-Za-z]{3,}$`); !re.MatchString(u.Surname) {
-		errMessage = append(errMessage, "Surname should consist of only English letters with a minimum length of 3 characters.")
-	}
-	if re := regexp.MustCompile(`^[A-Za-z0-9]{3,}$`); !re.MatchString(u.Username) {
-		errMessage = append(errMessage, "Username should consist of only English letters and digits with a minimum length of 3 characters.")
-	}
-	if re := regexp.MustCompile(`^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$`); !re.MatchString(u.Email) {
-		errMessage = append(errMessage, "Email should be in a valid format (e.g., john@example.com)")
-	}
-	if re := regexp.MustCompile(`^\+7[89]\d{9}$`); !re.MatchString(u.Phone) {
-		errMessage = append(errMessage, "Phone number should be validated according to specific phone number format requirements (example: +79251115599).")
-	}
-
-	return strings.Join(errMessage, "\n")
-}
+// TODO: convert to entity and UserRequest struct
